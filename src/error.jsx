9cd3c4b3d8fd276a247fccdaff422998c3199344
CyMaster <hyperpixel.team@outlook.com>
2024-07-08 15:48:31 +0100

    Update error.jsx

--- src/error.jsx
+++ src/error.jsx
@@ -5,7 +5,7 @@ function Error() {
     <>
         <Head defaultTitle="Error" />
         <div class="errorTitle">404 Error</div>
-        <div class="errorText">This page does not exist!</div>
+        <div class="errorText">This page does not exist! Or the requested URL/page could not be found. This error can also appear if you are using an insecure browser or opening Midnight inside of itself or another proxy. <a href="/">Go back to the Homepage</a></div>
     </>
   );
 }
