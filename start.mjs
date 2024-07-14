6774ec27ec7aa2865ae98371dec5c975889a8672
CyMaster <hyperpixel.team@outlook.com>
2024-07-14 05:44:24 +0000

    Update start.mjs

--- start.mjs
+++ start.mjs
@@ -25,24 +25,31 @@ const serve = serveStatic(fileURLToPath(new URL("./build/", import.meta.url)), {
 const fail = (req, res, err) => {
     res.writeHead(err?.statusCode || 500, {
         "Content-Type": "text/plain",
+        "X-Content-Type-Options": "nosniff",
+        "X-Frame-Options": "DENY",
+        "Content-Security-Policy": "default-src 'self'",
     });
     res.end(err?.stack);
 };
 
-httpServer.on("request", (req, res) => {
-    if (bareServer.shouldRoute(req)) {
-        bareServer.routeRequest(req, res);
-    } else {
-        serve(req, res, (err) => {
-            if (err?.code === "ENOENT") {
-                req.url = "/404.html";
-                res.statusCode = 404;
+httpServer.on("request", async (req, res) => {
+    try {
+        if (bareServer.shouldRoute(req)) {
+            bareServer.routeRequest(req, res);
+        } else {
+            serve(req, res, (err) => {
+                if (err?.code === "ENOENT") {
+                    req.url = "/404.html";
+                    res.statusCode = 404;
 
-                serve(req, res, (err) => fail(req, res, err));
-            } else {
-                fail(req, res, err);
-            }
-        });
+                    serve(req, res, (err) => fail(req, res, err));
+                } else {
+                    fail(req, res, err);
+                }
+            });
+        }
+    } catch (err) {
+        fail(req, res, err);
     }
 });
 
@@ -57,12 +64,12 @@ httpServer.on("upgrade", (req, socket, head) => {
 httpServer.on("listening", () => {
     const address = httpServer.address();
 
-    var theme = chalk.hex("#26233a");
+    const theme = chalk.hex("#26233a");
     console.log(`${chalk.bold(theme("Midnight"))}`);
 
     console.log(
         `  ${chalk.bold("Local:")}            http://${
-            address.family === "IPv6" ? `[${address.address}]` : addr.address
+            address.family === "IPv6" ? `[${address.address}]` : address.address
         }${address.port === 80 ? "" : ":" + chalk.bold(address.port)}`
     );
 
@@ -73,11 +80,14 @@ httpServer.on("listening", () => {
     );
 
     try {
-        console.log(
-            `  ${chalk.bold("On Your Network:")}  http://${address.ip()}${
-                address.port === 80 ? "" : ":" + chalk.bold(address.port)
-            }`
-        );
+        const networkAddress = getLocalNetworkAddress();
+        if (networkAddress) {
+            console.log(
+                `  ${chalk.bold("On Your Network:")}  http://${networkAddress}${
+                    address.port === 80 ? "" : ":" + chalk.bold(address.port)
+                }`
+            );
+        }
     } catch (err) {
         // can't find LAN interface
     }
@@ -99,6 +109,17 @@ httpServer.on("listening", () => {
     }
 });
 
-httpServer.listen({
-    port: port,
-});
+httpServer.listen({ port });
+
+// Function to get local network address
+function getLocalNetworkAddress() {
+    const interfaces = require('os').networkInterfaces();
+    for (const name of Object.keys(interfaces)) {
+        for (const iface of interfaces[name]) {
+            if (iface.family === 'IPv4' && !iface.internal) {
+                return iface.address;
+            }
+        }
+    }
+    return null;
+}
